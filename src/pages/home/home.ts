cb6d9cc317732a0b0154bb41e3ce3fe41e974467
Timothy Ngorima <timothytakudzwa@gmail.com>
2018-04-19 20:56:40 +0200

    My Sixth Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -40,9 +40,12 @@ public goToHomePage(){
 
 @ViewChild('map') mapElement: ElementRef;
 map: any;
+calls:any;
 lati: any = "";
 long: any = "";
 name: any;
+message: any;
+location : any;
 userData = {
     "longitude": "", "latitude": ""
 };
@@ -54,7 +57,7 @@ constructor(public navCtrl: NavController, public navParams: NavParams, public a
 loadMap() {
 
     let loader = this.loadingController.create({
-        content: "Please wait while map is loading....."
+        content: "Loading...."
     });
     loader.present();
     let _=this;
@@ -73,17 +76,73 @@ loadMap() {
            
 
         console.log(this.lati);
+            this.userData.latitude = this.lati;
+            this.userData.longitude = this.long;
+            this.authService.postData(this.userData, 'calls.php').then((result) => {
+
+                this.responseData = result;
+                console.log(this.responseData);
+                let message = localStorage.setItem('message', this.responseData.status_message);
+                let calls = localStorage.setItem('calls', this.responseData.phone);
+                let location = localStorage.setItem('location', this.responseData.location);
+                loader.dismiss();
+                
+            
+            }, (err) => {
+                // Error log
+            });
+
+
+
+
+
         loader.dismiss();
-        _.calling();
+        _.showConfirm();
     }, (err) => {
         console.log(err); 
     });
    
 }
+    showConfirm() {
+        let _ = this;
+        this.message = localStorage.getItem('message');
+        this.location = localStorage.getItem('location');
+        let confirm = this.alertCtrl.create({
+            title: 'Place Call',
+            message: this.message,
+            buttons: [
+
+                {
+                    text:  'Call ' + this.location + ' Police',
+                    handler: () => {
+                        _.calling();;
+                    }
+                },
+                {
+                    text: 'Choose Police Station',
+                    handler: () => {
+                        console.log('Agree clicked');
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    handler: () => {
+                        console.log('Agree clicked');
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
+
 calling(){
-this.callNumber.callNumber("0774231343", true)
+    this.calls = localStorage.getItem('calls');
+    console.log(this.calls);
+this.callNumber.callNumber(this.calls, true)
           .then(() => console.log('Launched dialer!'))
-          .catch(() => console.log('Error launching dialer'));  
+          .catch(() => console.log('Error launching dialer')); 
+          this.showConfirm(); 
 }
   login(){
       this.navCtrl.push(ReportsPage);
